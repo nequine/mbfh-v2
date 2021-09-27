544c920c79f4afcd7e7b2940ced1cb58664fceff
nequine <59351036+nequine@users.noreply.github.com>
2021-09-27 15:58:02 -0700

    Original plugin from Quixel
    Adding the original 4.4 for comparison purpose.
    
    All rights to the Quixel team.

---
+++ mbfh-v2/MSLiveLink/scripts/python/MSPlugin/MaterialsSetup/MaterialsCreator.py
@@ -0,0 +1,28 @@
+from ..Utilities.SingletonBase import Singleton
+
+class MaterialsCreator :
+    __metaclass__ = Singleton
+    def __init__(self):
+        self.materialMap = {}
+
+    def registerMaterial(self, rendererName, materialType, materialCreator):
+        if rendererName not in self.materialMap:
+            self.materialMap[rendererName] = {materialType : materialCreator}
+
+        else:
+            self.materialMap[rendererName].update({materialType:materialCreator})
+        
+
+    def createMaterial(self, rendererName, materialType, assetData, importParams, importOptions):
+        return self.materialMap[rendererName][materialType](assetData,importParams, importOptions)
+
+
+# Metaclass implementation that ensures implementation of a creator function
+class MaterialsBase:
+    def __init__(self):
+        pass
+
+
+
+
+
